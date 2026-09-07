59b7fadcb5d5062f4e5348c8937fff132d195a94
liberto <liberto.sharp@gmail.com>
2026-09-08 01:29:24 +0200

    Added some VectorExtensions utilities

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public static class VectorExtensions
 {
     public static Vector3 ToXZ(this Vector2 v) => new Vector3(v.x, 0, v.y);
+     public static Vector2 ToXY(this Vector3 v) => new Vector2(v.x, v.z);
     public static Vector2 SwapXY(this Vector2 v) => new Vector2(v.y, v.x);
     public static Vector3 WithY(this Vector3 v, float newY) => new Vector3(v.x, newY, v.z);
     public static Vector3 Avarage(this IEnumerable<Vector3> vecs)
@@ -19,4 +20,8 @@ public static class VectorExtensions
 
         return count == 0 ? Vector3.zero : result / count;
     }
+
+    public static Vector2Int ToInt(this Vector2 v) => new Vector2Int((int)v.x, (int)v.y);
+
+    public static Vector3 WithCenter(this Vector3 v, Vector3 center) => v - center;
 }
