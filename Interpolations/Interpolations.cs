0d0f8b5345f6d9d3ecb35d5b1f5cd290e5eeaa9e
liberto <liberto.sharp@gmail.com>
2026-09-09 11:45:46 +0200

    Added Interpolations coroutine Manager

--- Interpolations/Interpolations.cs
+++ Interpolations/Interpolations.cs
@@ -1,7 +1,51 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
-public static class Interpolations
+public static class Interpolations : LazySingleton<Interpolation>
 {
+	 public enum Type
+    {
+        LINEAR,
+        EASE_IN,
+        EASE_OUT,
+        SMOOTH_STEP
+    }
+
+    public void Reach(float start, float target, float seconds, Type type, Action<float> onUpdate)
+    {
+        StartCoroutine(ReachCoroutine(start, target, seconds, type, onUpdate));
+    }
+
+    private IEnumerator ReachCoroutine(float start, float target, float seconds, Type type, Action<float> onUpdate)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < seconds)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / seconds);
+            float easedT = ApplyEasing(t, type);
+            
+            onUpdate?.Invoke(Mathf.Lerp(start, target, easedT));
+            yield return null;
+        }
+
+        onUpdate?.Invoke(target);
+    }
+
+    private float ApplyEasing(float t, Type type)
+    {
+        return type switch
+        {
+            Type.LINEAR => t,
+            Type.EASE_IN => t * t,
+            Type.EASE_OUT => 1f - (1f - t) * (1f - t),
+            Type.SMOOTH_STEP => t * t * (3f - 2f * t),
+            _ => t
+        };
+    }
+	
     public static float Damp(float source, float target, float seconds, float dt)
     {
         float smoothing = Mathf.Pow(0.01f, 1 / seconds);
